74d10eb70bf66397cc5a615a9d61a72a7b05b4a7
Kellswork <kellogbonna@gmail.com>
2023-08-15 23:43:37 +0100

    all enpdoints built

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/render"
 	"github.com/thedevsaddam/renderer"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -29,11 +28,9 @@ var client *mongo.Client
 var db *mongo.Database
 
 const (
-	dbName         string = " todo-example"
+	dbName         string = "todo-example"
 	collectionName string = "todo"
 )
-
-// create const for database after you have seen where you use these values
 
 // step 3 create a todo model struct type fior the mongo db database and a todo struct type for the frontend
 type (
@@ -69,12 +66,11 @@ func init() {
 	checkError(err)
 	err = client.Ping(ctx, readpref.Primary())
 	checkError(err)
-	db = client.Database(dbName) // database name
+	db = client.Database(dbName)
 
 }
 
 func homeHandler(rw http.ResponseWriter, r *http.Request) {
-	// var reader io.Reader
 	filePath := "./README.md"
 	err := rnd.FileView(rw, http.StatusOK, filePath, "readme.md")
 	checkError(err)
@@ -85,7 +81,6 @@ func getTodos(rw http.ResponseWriter, r *http.Request) {
 	var todoListFromDB = []TodoModel{}
 	// fetch all the todos stored in the databse collection
 	filter := bson.D{}
-	// if the error is not equal to nil,
 	cursor, err := db.Collection(collectionName).Find(context.Background(), filter)
 	if err != nil {
 		log.Printf("failed to fetch todo records from the db: %v\n", err.Error())
@@ -96,16 +91,12 @@ func getTodos(rw http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
-	// if fetching the todo list was succesful
 	// create a todo list variable and assign it an empty array with type
-	// var todoList []todo{}
 	todoList := []Todo{}
 	if err = cursor.All(context.Background(), &todoListFromDB); err != nil {
 		checkError(err)
 	}
-	/* _, blank identifiier, somwwthing to note about range, they provide both index and value for each entry, we use the blank identifier when we don't need the index value
-	   range is used to iterate over elements in a variety of data structures
-	*/
+
 	// loop through the database array, convert to json using the todomodel and append to the todolist array.
 	for _, td := range todoListFromDB {
 		todoList = append(todoList, Todo{
@@ -115,8 +106,8 @@ func getTodos(rw http.ResponseWriter, r *http.Request) {
 			CreatedAt: td.CreatedAt,
 		})
 	}
-	// render a Json response for succesfully fetching the data.
-	// I used my own type getTodoResponse here instead of the renderer
+
+	// render a JSON response for successfully fetching the data with a custom getTodoResponse type
 	rnd.JSON(rw, http.StatusOK, GetTodoResponse{
 		Message: "All todos retrieved",
 		Data:    todoList,
@@ -124,7 +115,7 @@ func getTodos(rw http.ResponseWriter, r *http.Request) {
 }
 
 func createTodo(rw http.ResponseWriter, r *http.Request) {
-	// create variable t for storing user input
+	// create variable todo for storing user input
 	var todo Todo
 	// process the client input, if it returns an error , send a http response of bad request
 	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
@@ -134,15 +125,15 @@ func createTodo(rw http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
-	// check if the title is empty so check if the user semnt an empty title, return a response error message of title required
+	// check if the title is empty, return a response error message of title required
 	if todo.Title == "" {
 		log.Println("no title added to response body")
-		rnd.JSON(rw, http.StatusBadRequest, render.M{
+		rnd.JSON(rw, http.StatusBadRequest, renderer.M{
 			"message": "please add a title",
 		})
 		return
 	}
-	// if the error check passes, create a todomodel for adding a todo to the database
+	// create a todomodel for adding a todo to the database
 	todoModel := TodoModel{
 		ID:        primitive.NewObjectID(),
 		Title:     todo.Title,
@@ -162,7 +153,7 @@ func createTodo(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// if successfull, return a httpo status response success with the id.
-	rnd.JSON(rw, http.StatusOK, renderer.M{
+	rnd.JSON(rw, http.StatusCreated, renderer.M{
 		"message": "Todo created successfully",
 		"ID":      data.InsertedID,
 	})
@@ -171,10 +162,8 @@ func createTodo(rw http.ResponseWriter, r *http.Request) {
 func updateTodo(rw http.ResponseWriter, r *http.Request) {
 	// get the id from the url params
 	id := strings.TrimSpace(chi.URLParam(r, "id"))
-	fmt.Println("id: ", id)
 	// check if the id is a hex value because we stored it as a hex value, if error return a message with id invalid
 	res, err := primitive.ObjectIDFromHex(id)
-	fmt.Println("res: ", res)
 	if err != nil {
 		log.Printf("the id param is not a valid hex value: %v\n", err.Error())
 		rnd.JSON(rw, http.StatusBadRequest, renderer.M{
@@ -183,14 +172,12 @@ func updateTodo(rw http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
-	// create a todo object variable
+
 	var todo Todo
-	// decode the json body response, if it returns an error, return a json message and log error
 	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
 		log.Printf("failed to decode the json response body data: %v\n", err.Error())
 		rnd.JSON(rw, http.StatusInternalServerError, err.Error())
 	}
-	// check if the title is empty, return a json response if it is.
 	if todo.Title == "" {
 		rnd.JSON(rw, http.StatusBadRequest, renderer.M{
 			"message": "Title cannot be empty",
@@ -201,7 +188,7 @@ func updateTodo(rw http.ResponseWriter, r *http.Request) {
 	filter := bson.M{"id": res}
 	update := bson.M{"$set": bson.M{"title": todo.Title, "completed": todo.Completed}}
 	data, err := db.Collection(collectionName).UpdateOne(r.Context(), filter, update)
-	// if error, return a json response, if succesfull return a json response with the updated data
+
 	if err != nil {
 		log.Printf("failed to update db collection: %v\n", err.Error())
 		rnd.JSON(rw, http.StatusInternalServerError, renderer.M{
@@ -217,29 +204,23 @@ func updateTodo(rw http.ResponseWriter, r *http.Request) {
 }
 
 func deleteTodo(rw http.ResponseWriter, r *http.Request) {
-	// get the id from the url params
 	id := chi.URLParam(r, "id")
-	// check if id is a hex
 	res, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		log.Printf("invalid id: %v\n", err.Error())
 		rnd.JSON(rw, http.StatusBadRequest, err.Error())
 		return
 	}
-	// check if id exist in the database
-	// fetch tdod with that id
+
 	filter := bson.M{"id": res}
-	// options := bson.M{"$set": bson.M{"title": todo.Title, "completed": todo.Completed}}
 	// delete that todo entry in the database
 	if data, err := db.Collection(collectionName).DeleteOne(r.Context(), filter); err != nil {
-		// if error return a 500 http status and the error
 		log.Printf("could not delete item from database: %v\n", err.Error())
 		rnd.JSON(rw, http.StatusInternalServerError, renderer.M{
 			"message": "an error eccoured while deleting todo item",
 			"error":   err.Error(),
 		})
 	} else {
-		// if successfull, return a json response message with http status ok
 		rnd.JSON(rw, http.StatusOK, renderer.M{
 			"message": "item deleted successfully",
 			"data":    data,
@@ -249,21 +230,21 @@ func deleteTodo(rw http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// step 6 create router and route handlers for home
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", homeHandler)          // for homepage and todo routes
-	r.Mount("/todo", todoHandlers()) // Mount attaches another http.Handler along ./pattern/*
+	// step 8 create router and route handlers for home
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", homeHandler)          // for homepage and todo routes
+	router.Mount("/todo", todoHandlers()) // Mount attaches another http.Handler along ./pattern/*
 
-	// step 7 connect to a server
+	// step 6 connect to a server
 	server := &http.Server{
 		Addr:         ":9000",
-		Handler:      r,
+		Handler:      router,
 		ReadTimeout:  60 * time.Second,
 		WriteTimeout: 60 * time.Second,
 	}
 
-	// step 8: stop the server
+	// step 7: stop the server
 	// create channel to reccieve signal
 	stopChan := make(chan os.Signal, 1)
 	signal.Notify(stopChan, os.Interrupt)
@@ -281,7 +262,6 @@ func main() {
 	log.Printf("shutting down server: %v\n", sig)
 	if err := client.Disconnect(context.Background()); err != nil {
 		panic(err)
-
 	}
 
 	// create a context with a timeout
@@ -298,14 +278,14 @@ func main() {
 
 // step 9: create a group route for todo routers
 func todoHandlers() http.Handler {
-	rg := chi.NewRouter()
-	rg.Group(func(r chi.Router) {
+	router := chi.NewRouter()
+	router.Group(func(r chi.Router) {
 		r.Get("/", getTodos)
 		r.Post("/", createTodo)
 		r.Put("/{id}", updateTodo)
 		r.Delete("/{id}", deleteTodo)
 	})
-	return rg
+	return router
 }
 
 // step 5 define checkErroro function
